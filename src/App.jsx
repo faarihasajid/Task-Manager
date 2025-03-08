f67ff92aeb4d87719495bceaafd5c51e8f97b6e0
faarihasajid <faarihakhokhar@gmail.com>
2025-03-08 12:28:34 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Home from './Components/Home';
+import 'antd/dist/reset.css'; 
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>
+  );
+}
+
+export default App;
